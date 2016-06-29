1e8293bfba721307435c8ba3a53a89879545b781
Weilin Luo <william_luo2004@yahoo.com>
2016-06-28 18:37:50 -0700

    wk2: computeCost.m

--- wk2/ex1/computeCost.m
+++ wk2/ex1/computeCost.m
@@ -13,6 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% predictions is m * 1 vector which is h_theta(x): predicted value using current theta parameters
+predictions = X * theta	
+
+% use the predictions to compute J(theta)
+J = (1/2*m) * sum((predictions - y) .^ 2)
 
 
 
